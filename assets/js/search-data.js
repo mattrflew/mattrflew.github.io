a89f5b25134942853f995e9cf8e7798c299d6c7f
mattrflew <151459551+mattrflew@users.noreply.github.com>
2025-11-28 18:37:41 +0000

    Deploying to gh-pages from @ mattrflew/mattrflew.github.io@44b724821c13ec325604ee85ce9de4ce884a6430 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-projects",
           title: "projects",
-          description: "A few things I&#39;ve worked on. More on the way! Test",
+          description: "A few things I&#39;ve worked on. More on the way!",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
