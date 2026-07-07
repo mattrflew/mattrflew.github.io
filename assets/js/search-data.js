96c578e3c1ce346e0b5615d2c3bd701237bebfd2
mattrflew <151459551+mattrflew@users.noreply.github.com>
2026-07-07 23:51:39 +0000

    Deploying to gh-pages from @ mattrflew/mattrflew.github.io@6c14a7897d637ea95371376e95ebcee537c824be 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-resume",
-          title: "resume",
-          description: "Click PDF to the right for a full resume.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/resume/";
-          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
